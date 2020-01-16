c2a39a1af65dcaa01afcacbf5cf872d0b066ed1a
yugguo <yugguo@adobe.com>
2020-01-16 11:26:07 -0800

    initialization

---
+++ MovieRaterApi/api/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            user = request.user
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                print('update')
+
+                serializer = RatingSerializer(rating, many=False)
+
+                response = {'message': 'rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+            except:
+                Rating.objects.create(user=user, movie=movie, stars = stars)
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            response = {'message': 'you need to add data'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
---
+++ MovieRaterApi/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/Data/Yi/Udemy/MovieRaterApi/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
